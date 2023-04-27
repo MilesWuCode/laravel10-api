e4b5891b9b23068a6f8dec3f67c40c4c889b3a4c
Miles Wu <slion0725@gmail.com>
2023-04-27 23:39:53 +0800

    upd

--- app/DTO/PostDTO.php
+++ app/DTO/PostDTO.php
@@ -8,9 +8,6 @@ use Illuminate\Http\Request;
 
 class PostDTO
 {
-    /**
-     * 4.使用DTO建立資料
-     */
     public function __construct(
         public readonly string $title,
         public readonly string $content
@@ -19,6 +16,12 @@ class PostDTO
 
     public static function create(PostStoreRequest $request): PostDTO
     {
+        /**
+         * 4.使用DTO建立資料
+         * 請求的資料可以在DTO層變換
+         * ex: title: $request->name,
+         * ex: age: $nowYear - $request->year,
+         */
         return new self(
             title: $request->title,
             content: $request->content,
