3c7457128196535b0b35a1564bd5d92f99649a03
Miles Wu <slion0725@gmail.com>
2023-05-30 17:16:00 +0800

    upd

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -32,3 +32,58 @@ it('登入', function () {
     // Assert
     $response->assertStatus(200);
 });
+
+it('寄驗證信/驗證代碼', function () {
+    // Prepare
+    $user = User::factory()->create();
+
+    $user->email_verified_at = null;
+
+    $user->save();
+
+    $form = ['email' => $user->email];
+
+    // Act
+    $response = $this->post('/api/auth/send-verify-email', $form);
+
+    // Assert
+    $response->assertStatus(200);
+
+    $verify = $user->verifies()
+        ->where('expires', '>=', now())
+        ->first();
+
+    $form = ['email' => $user->email, 'code' => $verify->code];
+
+    $response = $this->post('/api/auth/verify-email', $form);
+
+    $response->assertStatus(200);
+});
+
+it('忘記密碼/變更密碼', function () {
+    // Prepare
+    $user = User::factory()->create();
+
+    $form = ['email' => $user->email];
+
+    // Act
+    $response = $this->post('/api/auth/forgot-password', $form);
+
+    // Assert
+    $response->assertStatus(200);
+
+    $verify = $user->verifies()
+        ->where('expires', '>=', now())
+        ->first();
+
+    $form = [
+        'email' => $user->email,
+        'password' => 'password',
+        'comfirm_password' => 'password',
+        'code' => $verify->code,
+    ];
+
+    $response = $this->post('/api/auth/reset-password', $form);
+
+    $response->assertStatus(200);
+});
