624a0d399e627d783caec92a1a9b53ee67e1b8ad
Miles Wu <slion0725@gmail.com>
2023-06-17 17:28:27 +0800

    upd

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -30,6 +30,12 @@ class PostRepository
         $cache = Cache::remember('post.list.'.$queryString, 600, fn () =>
             // 取資料
             Post::with('user')
+                ->with([
+                    'loveReactant.reactions.reacter.reacterable',
+                    'loveReactant.reactions.type',
+                    'loveReactant.reactionCounters',
+                    'loveReactant.reactionTotal',
+                ])
                 ->paginate($limit) // 每頁幾筆資料
                 // ->simplePaginate(5) // 不提供頁數號碼只提供上一頁跟下一頁
                 // ->cursorPaginate(5, ['*'], 'page') // 座標
