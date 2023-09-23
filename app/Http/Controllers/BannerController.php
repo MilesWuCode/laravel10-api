83da6767adb06742502ca5447c19e3297a6ad783
Miles Wu <slion0725@gmail.com>
2023-09-23 14:53:35 +0800

    upd

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -4,14 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\BannerCollection;
 use App\Services\BannerService;
+use Illuminate\Support\Facades\Cache;
 
 class BannerController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(BannerService $bannerService): BannerCollection
+    public function index(BannerService $bannerService)
     {
-        return new BannerCollection($bannerService->list());
+        // 不用快取時
+        // return new BannerCollection($bannerService->list());
+
+        return Cache::remember('banner.index', 300, function () use($bannerService) {
+            // 快取物件，使用快取時會被再執行一次裡面的函式
+            // return new BannerCollection($bannerService->list());
+
+            // 需要先轉成response再做快取
+            $collection = new BannerCollection($bannerService->list());
+
+            return $collection->response();
+        });
     }
 }
